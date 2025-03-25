f7909f323d837fe83fe47c8fdf4159aa7fcb9682
G3nj1X <lucas.tagsprate@yahoo.fr>
2025-03-25 23:58:39 +0100

    Premier commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function changeColor() {
+    let body = document.body;
+    if (body.style.backgroundColor === "blue") {
+        body.style.backgroundColor = "red";
+    } else {
+        body.style.backgroundColor = "blue";
+    }
+}
